00c6e836bc05a4d4098df424bed5ebeeec8ed470
root <irunguboniface413@gmail.com>
2024-10-31 09:04:02 +0000

    0x01-caching

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" BaseCache module
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ BasicCache defines:
+      - inherts from BaseCaching
+      - constants of your cache system
+      - where your data are stored (in a dictionary)
+    """
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        return self.cache_data.get(key)
